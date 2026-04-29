159929a89fda76e5fa9bf7884d4d582fd4006a94
Luwisiii <paoskidumayas09@gmail.com>
2026-04-29 21:13:59 +0800

    Fixed Start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,7 @@
 #!/usr/bin/env bash
 
-echo "REDIS_URL=$REDIS_URL"
+printenv | grep REDIS_URL
 
-# wait a moment for env to be ready
-sleep 3
-
-# start celery in background
 celery -A app.core.celery_app worker --loglevel=info &
 
-# start fastapi
 uvicorn app.main:app --host 0.0.0.0 --port 10000
